bea3afff9d35c493872ed8abc5bdbda3640a78d6
ismoil793 <ismoil.793@gmail.com>
2020-01-01 15:17:37 +0500

    Power asian groups final

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,14 +35,12 @@ $(function () {
                 "                    или тороидальными. Все три типа трансформаторов имеют похожие\n" +
                 "                    характеристики, и надежность, но отличаются друг от друга способом\n" +
                 "                    изготовления");
-        }
-        else if (product_titleText.startsWith('Изоляционные')) {
+        } else if (product_titleText.startsWith('Изоляционные')) {
 
             $('.product-info').html("Изоляционные материалы\n бывают: стержневыми, броневыми\n" +
                 "                    Lorem ipsum dolor sit amet, consectetur adipisicing " +
                 "elit. Aut commodi repellendus velit?");
-        }
-        else if (product_titleText.startsWith('Реакторы')) {
+        } else if (product_titleText.startsWith('Реакторы')) {
 
             $('.product-info').html("Реакторы Lorem ipsum dolor sit amet, consectetur adipisicing " +
                 "elit. Aut commodi repellendus velit?");
@@ -54,27 +52,92 @@ $(function () {
 
     // $("#menu").mmenu();
     $("#menu").mmenu({
-       "extensions": [
-       "pagedim-black",
-       ],
-       "navbars": [
-       {
-         "position": "top",
-         // "content": [
-         // "searchfield"
-         // ]
-     },
-     {
-         "position": "bottom",
-         "content": [
-         "<a class='fab fa-facebook' href='#/'></a>",
-         "<a class='fab fa-instagram' href='#/'></a>",
-         "<a class='fab fa-telegram' href='#/'></a>"
-         ]
-     }
-     ]
- });
+        "extensions": [
+            "pagedim-black",
+        ],
+        "navbars": [
+            {
+                "position": "top",
+                // "content": [
+                // "searchfield"
+                // ]
+            },
+            {
+                "position": "bottom",
+                "content": [
+                    "<a class='fab fa-facebook' href='#/'></a>",
+                    "<a class='fab fa-instagram' href='#/'></a>",
+                    "<a class='fab fa-telegram' href='#/'></a>"
+                ]
+            }
+        ]
+    });
 
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 30) {  //$(window).height()
+            $('.hamburger-logo').addClass('home-page-menu-style')
+        } else {
+            $('.hamburger-logo').removeClass('home-page-menu-style')
+        }
+    });
+
+
+    //wow js
+    new WOW().init();
+
+
+    // Кнопка "Наверх"
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > $(window).height()) {
+            $('.top-button').addClass('active');
+        } else {
+            $('.top-button').removeClass('active');
+        }
+    });
+    $('.top-button').click(function () {
+        $('html,body').stop().animate(
+            {scrollTop: 0}, 'slow', 'swing'
+        );
+    });
+
+
+    // Select all links with hashes
+    $('a[href*="#news-block-title"]')
+    // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        }
+                        ;
+                    });
+                }
+            }
+        });
 });
 
 $(window).on('load', function () {
